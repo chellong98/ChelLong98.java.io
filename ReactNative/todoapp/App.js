71802383567b848d0564e1edeb0423f2799b7f0c
chellong7798 <nhatlong@hocvienact.edu.vn>
2018-04-06 18:30:04 +0700

    new code 18:29 6/4

--- ReactNative/todoapp/App.js
+++ ReactNative/todoapp/App.js
@@ -1,32 +1,3 @@
-import React from 'react';
-import {StackNavigator} from 'react-navigation';
-import SplashScreenContainer from './src/containers/splashsreenContainer';
-import TodosContainer from './src/containers/todoContainer';
-import NewTodoContainer from './src/containers/NewTodoContainer';
-import EditTodoContainer from './src/containers/EditTodoContainer';
-import RealmContainer from './src/containers/RealmContainer';
-const App = StackNavigator(
-  {
-    SplashScreenContainer : {
-      screen: SplashScreenContainer
-    },
-    TodosContainer: {
-      screen: TodosContainer,
-    },
-    NewTodoContainer: {
-      screen: NewTodoContainer
-    },
-    EditTodoContainer: {
-      screen: EditTodoContainer
-    },
-    RealmContainer: {
-      screen : RealmContainer
-    }
-  },
-  {
-    initialRouteName: "SplashScreenContainer",
-    headerMode: "none",
-  }
-)
-
-export default App;
+import boot from './src/boot/index';
+const app = boot()
+export default app;
